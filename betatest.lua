7c65f36eac6cc4b3d0f7f0a098b1a57807150644
BURIKATSS <141358289+Erza-Proxy@users.noreply.github.com>
2025-10-13 04:02:27 +0800

    Update betatest.lua

--- betatest.lua
+++ betatest.lua
@@ -1,11 +1,23 @@
+-- Simple test fetch with debug
 local version = "mp"  -- or "betatest"
+
+-- Fetch Lua code from server
 local res_obj = makeRequest(
-    "https://yourserver.com/get_lua_file?version=" .. version,
+    "https://erza.pythonanywhere.com/get_lua_file?version=" .. version,
     "GET",
     { {"User-Agent", "ErzaProxyOnTop"} },
     "",
     5000
 )
 
-logToConsole("Response content: " .. tostring(res_obj.content))
-logToConsole("Response length: " .. tostring(#res_obj.content))
+-- Debug outputs
+logToConsole("HTTP response object: " .. tostring(res_obj))
+logToConsole("Response content length: " .. tostring(res_obj.content and #res_obj.content or 0))
+logToConsole("Response preview (first 200 chars): " .. (res_obj.content and res_obj.content:sub(1,200) or "nil"))
+
+-- Check if server returned Unauthorized
+if res_obj.content and res_obj.content:find("Unauthorized") then
+    logToConsole("Server response: Unauthorized")
+else
+    logToConsole("Server response looks okay")
+end
