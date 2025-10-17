cbb24682d995cbf400e4174889f3cb40aa9a070e
BURIKATSS <141358289+Erza-Proxy@users.noreply.github.com>
2025-10-17 16:14:25 +0800

    Update NEWCP.lua

--- NEWCP.lua
+++ NEWCP.lua
@@ -1,5 +1,5 @@
 return {
-    [""] = { discordid = "1", version = "mp", expires = 1770900347412 },
+    ["c942b4262dad878a32aefdca110da9efb0ed184daaebdacc585326d0db2e7112"] = { discordid = "1176086017648902234", version = "mp", expires = 1763239200000 },
     ["40ee3906474603a69a16253239be7dc168c8f2547fc8c29efdb52c25067c2c36"] = { discordid = "1425781810218143744", version = "mp", expires = 1763239200000 },
     [""] = { discordid = "1384492381323399262", version = "mp", expires = 1763251200000 },
     [""] = { discordid = "1073853554978205759", version = "mp", expires = 1763251200000 },
